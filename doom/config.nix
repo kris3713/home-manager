ee8b63e325406161d2f7063cb5c6b6345981480c
kris3713 <07e5297d5b@c0x0.com>
2026-01-18 14:07:59 -0700

    Update Doom Emacs config
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- doom/config.nix
+++ doom/config.nix
@@ -1,7 +1,8 @@
-_: {
+{ pkgs, ... }: {
   # DOOM Emacs
   programs.doom-emacs = {
     enable = true;
+    emacs = pkgs.emacs-pgtk;
     doomDir = ./.;
     extraPackages = epkgs: with epkgs; [
       catppuccin-theme
