bf2944b3e3bfb5136a34e1647da49e5a726fe0e6
kris3713 <07e5297d5b@c0x0.com>
2026-04-05 15:14:08 -0600

    Updates
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- home.nix
+++ home.nix
@@ -22,11 +22,12 @@ in {
   nix = {
     package = determinateNix.nix;
     settings = {
+      always-allow-substitutes = true;
+      extra-allowed-users = [ username ];
       extra-substituters = [ "https://cache.numtide.com" ];
       extra-trusted-public-keys = [
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
-      extra-allowed-users = [ username ];
       extra-trusted-users = [ username ];
     };
   };
